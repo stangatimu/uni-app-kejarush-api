eb5751876aab478ef193fc019f7ad55b293de5be
stan gatimu <stangatimu@gmail.com>
2019-03-02 18:11:17 +0300

    admin manage agent controller

--- api/controllers/agent.js
+++ api/controllers/agent.js
@@ -80,33 +80,69 @@ exports.agent_login = (req,res)=>{
 
 exports.agent_edit = async (req,res)=>{
     try{
-        agent = await Agent.find({_id: req.userData._id});
+        const agent = await Agent.findOne({_id: req.userData.agentID});
+        if(!agent){
+            return res.status(404).json({
+                success: false,
+                message: 'the specified agent does not exist'
+            });
+        }
+        req.body.name ? agent.name = req.body.name : agent.name;
+        if(req.body.password){    
+                hash = await bcrypt.hash(req.body.password,10);
+                agent.password = hash;
+        }
+        agent.save();
+        return res.status(201).json({
+            success: true,
+            message: 'agent was successfuly updated.',
+            agent:{
+                name: agent.name,
+            }
+        });
+
     }catch(err){
         return res.status(500).json({
             success: false,
-            message:'error while editing agent'
+            message:err.message
         });
-    }
-    if(!agent){
-        return res.status(404).json({
+    } 
+}
+
+exports.agent_admin_edit = async (req,res)=>{
+    if(req.userData.role !== 'admin'){
+        return res.status(401).json({
             success: false,
-            message: 'the specified agent does not exist'
-        });
+            message: "You are not allowed to perform this action."
+        });         
     }
-
-    req.body.name ? agent.name = req.body.name : agent.name;
-
-    if(req.body.password){
-        try{
-
-            hash = bcrypt.hash(req.body.password,16);
-            agent.name = hash;
-
-        }catch(err){
-            return res.status(500).json({
+    try{
+        const agent = await Agent.findOne({_id: req.userData.agentID});
+        if(!agent){
+            return res.status(404).json({
                 success: false,
-                message: 'error occured while editing'
+                message: 'the specified agent does not exist'
             });
         }
+        req.body.status ? agent.status = req.body.status: agent.status;
+        req.body.role ? agent.role = req.body.role : agent.role;
+
+        agent.save();
+        return res.status(201).json({
+            success: true,
+            message: 'agent was successfuly updated.',
+            agent:{
+                name: agent.name,
+                _id: agent._id,
+                role: agent.role,
+                status: agent.status
+            }
+        });
+
+    }catch(err){
+        return res.status(500).json({
+            success: false,
+            message:err.message
+        });
     }
 }
