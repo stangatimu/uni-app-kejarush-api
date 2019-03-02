618feee56bdba1b7b551559ce5b3a4b87ce8f85c
stan gatimu <stangatimu@gmail.com>
2019-03-02 14:57:43 +0300

    check role  added

---
+++ api/utils/agent/check_role.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = async (req, res,next)=>{
+	try {
+		const token = req.headers.authorization.split(" ")[1];
+        const decoded = jwt.verify(token, process.env.jwtkey);
+        
+        if(decoded.role == 'agent' || decoded.role == 'admin'){
+            req.userData = decoded;
+			next();            
+        }else{
+            res.status(401).json({
+                success: false,
+                message:"You are not authorized to perform this actions."
+            });
+            
+        }
+        
+
+	} catch (err) {
+		res.status(401).json({
+			message: 'Authentication failed, please login to continue.'
+		});
+	}
+	
+}
