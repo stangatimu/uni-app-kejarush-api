f120e1cf725b1efc1f7eb393946930b20d3bd700
stan gatimu <stangatimu@gmail.com>
2019-03-03 19:50:21 +0300

    added ad create route

---
+++ api/routes/ad.js
@@ -0,0 +1,9 @@
+const express = require('express'),
+    router = express.Router(),
+    Controllers = require('../controllers/ad'),
+    rateLimit = require('../utils/rate_limit'),
+    checkRole = require('../utils/agent/check_role');
+
+
+
+router.post('/', rateLimit.globalBF.prevent,checkRole,Controllers.ad_create);
