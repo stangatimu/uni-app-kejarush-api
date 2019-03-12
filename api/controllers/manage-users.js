9965ab45b06609259153de6d6144a5a5bd46a4e1
stan gatimu <stangatimu@gmail.com>
2019-03-12 22:26:32 +0300

    added manage user file

---
+++ api/controllers/manage-users.js
@@ -0,0 +1,31 @@
+const User = require('../models/user'),
+    Property = require('../models/property');
+
+exports.agent_asign_house = (req,res)=>{
+    //validate input
+
+    //check user and property
+
+    //credit user with deposit and  1st rent
+
+    //asign property to user and save
+    
+    //return success
+
+    //catch any error
+}
+
+exports.agent_revoke_house = (req,res)=>{
+    // validate input
+
+    //look up user
+
+    //edit property field and set rent field 0
+
+    //catch any error
+}
+
+exports.agent_edit_rent = (req,res)=>{
+    
+}
+
