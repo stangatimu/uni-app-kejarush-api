7f6d04697d304055c7d98f34a4b80988683db116
stan gatimu <stangatimu@gmail.com>
2019-04-01 22:53:07 +0300

    added get payments route

--- api/routes/payments.js
+++ api/routes/payments.js
@@ -5,9 +5,11 @@ const express = require('express'),
     Controllers = require('../controllers/payments');
 
 
-router.post('/payrent',rateLimit.userBF.prevent,checkAuth,Controllers.init_Payment);
+router.post('/payrent',rateLimit.userBF.prevent,checkAuth,Controllers.init_Payment)
 
-router.post('/callbackUrl',rateLimit.globalBF.prevent,checkAuth,Controllers.payment_callback);
+router.post('/callbackUrl',rateLimit.globalBF.prevent,checkAuth,Controllers.payment_callback)
+
+router.get('/payments',rateLimit.globalBF.prevent,checkAuth,Controllers.get_user_payments)
 
 
 module.exports = router
